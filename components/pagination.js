e7454003928fa25176e49712620b35874ab614cd
Santosh2740 <santoshvishwa1572@gmail.com>
2022-06-20 15:04:23 +0530

    Done

---
+++ components/pagination.js
@@ -0,0 +1,26 @@
+function pagination(){
+    return `
+    <div id="pagination">    
+        <a href="#" class="active" style="background-color: gray;" >First </a>
+        <a href="#" class="active" style="background-color: gray;">Prev</a>
+        <a href="Home.html" class="active">1</a>
+        <a href="Home.html">2</a>
+        <a href="details.html">3</a>
+        <a href="searchFdetails.html">4</a>
+        <a href="searchFdetails.html">5</a>
+        <a href="#">6</a>
+        <a href="#">7</a>
+        <a href="#">8</a>
+        <a href="#">9</a>
+        <a href="#">10</a>
+        <a href="#">11</a>
+        <a href="#">12</a>
+        <a href="#">13</a>
+        <!-- <a href="#">14</a> -->
+        <!-- <a href="#">15</a> -->
+        <a href="#" class="active">NEXT</a>
+    </div> 
+    `
+}
+
+export {pagination}
